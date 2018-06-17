a0b2e37579511ce8a298be5507879cadf0f229f3
NinjawarriorMP <32735104+NinjawarriorMP@users.noreply.github.com>
2018-06-17 02:19:00 -0400

    added functionality to display recipe model data and added sample data for testing

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Chicken Tikka Masala',
+      'Favorite Indian Dish!',
+      'https://c1.staticflickr.com/6/5705/20586696920_6f6effb898_b.jpg'),
+    new Recipe('Chicken Tikka Masala',
+      'Favorite Indian Dish!',
+      'https://c1.staticflickr.com/6/5705/20586696920_6f6effb898_b.jpg')
+
+  ];
 
   constructor() { }
 
