ae98e945e11fdfda20d9c01930ac173692007c70
NinjawarriorMP <32735104+NinjawarriorMP@users.noreply.github.com>
2018-06-29 12:07:06 -0400

    added routes and changed navigation based on routes

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,17 +1,10 @@
-import {Component, EventEmitter, Output} from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
 export class HeaderComponent {
-  @Output() viewSelection = new EventEmitter<{displayType: string}>();
 
-  onRecipesSelected() {
-    this.viewSelection.emit({displayType: 'recipe'});
-  }
 
-  onShoppingListSelected() {
-    this.viewSelection.emit({displayType: 'shoppingList'});
-  }
 }
