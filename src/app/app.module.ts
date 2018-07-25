cbedf611f83b855554078107457a734c9bb3f492
NinjawarriorMP <32735104+NinjawarriorMP@users.noreply.github.com>
2018-07-25 00:10:55 -0400

    added signup component with routing which will be used for user signup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,6 +19,8 @@ import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component
 import {RecipeService} from './recipes/recipe.service';
 import {HttpModule} from '@angular/http';
 import {DataStorageService} from './shared/data-storage.service';
+import { SignupComponent } from './auth/signup/signup.component';
+import { SigninComponent } from './auth/signin/signin.component';
 
 @NgModule({
   declarations: [
@@ -33,6 +35,8 @@ import {DataStorageService} from './shared/data-storage.service';
     DropdownDirective,
     RecipeStartComponent,
     RecipeEditComponent,
+    SignupComponent,
+    SigninComponent,
   ],
   imports: [
     BrowserModule,
